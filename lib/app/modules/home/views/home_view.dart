bd00b69ad433839de996e49e91930844f0167be5
Your Name <info.batteryqk@gmail.com>
2025-09-18 10:56:33 +0600

    search box add

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,22 +3,47 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+import '../widget/custome_location_row.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              CustomLocationRow(
+                selectedLocation: "Dhaka",
+                locations: ["Dhaka", "Chittagong", "Khulna"],
+                onLocationChanged: (value) {
+                  print("Selected: $value");
+                },
+                onNotificationTap: () {
+                  print("Notification tapped!");
+                },
+              ),
+              SearchBar(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+
+                elevation: WidgetStateProperty.all(0.0),
+                leading: Icon(Icons.search),
+                hintText: "Search for food",
+                onChanged: (value) {
+                  print("Searching: $value");
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
+
   }
 }
