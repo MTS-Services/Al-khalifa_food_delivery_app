662967b00af86f01dc3b3ed259ae7e5bce9cb06c
remon <remonhowlader97@gmail.com>
2025-09-18 10:54:58 +0600

    screenutil and data file has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:al_khalifa/app/data/theme.dart';
+
 import 'package:al_khalifa/app_management.dart';
 import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(AppManagement());
